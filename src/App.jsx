b20c055b5ac569da89a027626054aa704a56ff83
aladdinalizada <tu7c5zneb@code.edu.az>
2023-12-03 00:48:58 +0400

    install and configure react-helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import MainPage from "./pages/MainPage";
+import { Helmet } from "react-helmet";
+import MainPage from "./pages/Home/MainPage";
 
 function App() {
   return (
     <>
       <div className="w-full h-full">
+        <Helmet>
+          <title>LaslesVPN | Home</title>
+          <meta name="description" content="LaslesVPN Homepage" />
+          <meta name="keywords" content="LaslesVPN, VPN, Homepage" />
+          <meta name="author" content="LaslesVPN" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Helmet>
         <MainPage />
       </div>
     </>
